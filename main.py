a18a06ad0aa93f3fef2f405032a5aab4f1d0a8a8
TokyoGaijin <120317383+TokyoGaijin@users.noreply.github.com>
2023-09-10 23:31:13 +0900

    Menu Bar Staged

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ class SystemMenuBar:
             self.current_color = self.unselected_color
             self.rect = pygame.Rect(posX, posY, width, 30)
             self.is_mouse_over = False
-            
+                        
         def on_click(self):
             pyautogui.alert("Under Construction")
             
@@ -58,29 +58,85 @@ class SystemMenuBar:
             text = self.font.render(self.menu_name, True, self.current_font_color)
             SURFACE.blit(text, (self.posX + 5, self.posY + 10))
             
+    class AppsMenu(SubMenu):
+        def __init__(self, posX, posY, menu_name, width = 35):
+            super().__init__(posX, posY, menu_name, width)
+            self.apps_icon = pygame.image.load(os.path.join(BASE_DIR, "png_icons", "mac.png"))
+            self.selected_icon = pygame.image.load(os.path.join(BASE_DIR, "png_icons", "mac_selected.png"))
+            self.current_icon = self.apps_icon
+            self.rect = pygame.Rect(posX, posY, self.apps_icon.get_width(), self.apps_icon.get_height())
+            
+        def update(self):
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if self.rect.collidepoint(mouse_x, mouse_y):
+                self.is_mouse_over = True
+                if pygame.mouse.get_pressed()[0]:
+                    self.on_click()
+            else:
+                self.is_mouse_over = False
+                
+            if self.is_mouse_over:
+                self.current_icon = self.selected_icon
+            else:
+                self.current_icon = self.apps_icon
+            
+        def draw(self):
+            SURFACE.blit(self.current_icon, (self.posX, self.posY))
+            
+    class SystemMenu(SubMenu):
+        def __init__(self, posX, posY, menu_name, width = 57):
+            super().__init__(posX, posY, menu_name, width)
+            ## TODO: Add drop-down menu
+            
+        ## TODO: Add new on_click command
+        
+    class RunMenu(SubMenu):
+        def __init__(self, posX, posY, menu_name, width = 35):
+            super().__init__(posX, posY, menu_name, width)
+            ## TODO: Add drop-down menu
+            
+        ## TODO: add new on_click command
+    
+    class SettingsMenu(SubMenu):
+        def __iniit__(self, posX, posY, menu_name, width = 75):
+            super().__init__(posX, posY, menu_name, width)
+            ## TODO: Add drop-down menu
+            
+        ## TODO: add new on_click command    
+        
+        
+    class HelpMenu(SubMenu):
+        def __init__(self, posX, posY, menu_name, width = 38):
+            super().__init__(posX, posY, menu_name, width)
+            ## TODO: Add drop-down Menu
+            
+        ## TODO: add new on_click command
+            
+                
     class ShutDownCommand(SubMenu):
-        def __init__(self, posX, posY, menu_name, width = 30):
+        def __init__(self, posX, posY, menu_name, width = 80):
             super().__init__(posX, posY, menu_name, width)
             
         def on_click(self):
             result = pyautogui.confirm("Are you sure you want to shut down?", buttons=["Yes", "No"])
             if result == "Yes":
                 exit(1)
+                
             
             
         
     def __init__(self, posX, posY):
         self.posX = posX
         self.posY = posY
-        self.system_menu = self.SubMenu(self.posX + 70, self.posY, "System", width = 57)
-        self.run_menu = self.SubMenu(self.posX + 70 + self.system_menu.rect.width, self.posY, "Run", width = 35)
-        self.settings_menu = self.SubMenu(self.posX + 70 + self.system_menu.rect.width + self.run_menu.rect.width, self.posY, "Settings", width = 65)
-        self.help_menu = self.SubMenu(self.posX + 70 + self.system_menu.rect.width + self.run_menu.rect.width + self.settings_menu.rect.width, self.posY, "Help", width = 38)
-        self.shut_down_menu = self.ShutDownCommand(self.help_menu.posX + self.help_menu.rect.width, self.posY, "Shut Down", width = 80)
+        self.apps_menu = self.AppsMenu(30, 0, "Apps")
+        self.system_menu = self.SystemMenu(self.posX + 70, self.posY, "System")
+        self.run_menu = self.RunMenu(self.system_menu.posX + self.system_menu.rect.width, self.posY, "Run")
+        self.settings_menu = self.SettingsMenu(self.run_menu.posX + self.run_menu.rect.width, self.posY, "Settings", width = 65)
+        self.help_menu = self.HelpMenu(self.settings_menu.posX + self.settings_menu.rect.width, self.posY, "Help")
+        self.shut_down_menu = self.ShutDownCommand(self.help_menu.posX + self.help_menu.rect.width, self.posY, "Shut Down")
         self.rect = pygame.Rect(posX, posY, SCREEN_X, self.system_menu.rect.height)
         self.color = self.system_menu.unselected_color
         
-        #Teehee
         
     def update(self):
         self.system_menu.update()
@@ -88,10 +144,12 @@ class SystemMenuBar:
         self.settings_menu.update()
         self.help_menu.update()
         self.shut_down_menu.update()
+        self.apps_menu.update()
         
         
     def draw(self):
         pygame.draw.rect(SURFACE, self.color, self.rect)
+        self.apps_menu.draw()
         self.system_menu.draw()
         self.run_menu.draw()
         self.settings_menu.draw()
